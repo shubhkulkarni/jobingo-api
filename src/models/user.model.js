c2c2effe0104f7f57b59d81b8fc71c237876c588
Shubham Kulkarni <58355613+shubhkulkarni@users.noreply.github.com>
2022-03-17 17:00:45 +0530

    models

---
+++ src/models/user.model.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
+
+const userSchema = new Schema({
+  name: { type: String, required: [true, "name is required"], minlength: 3 },
+  email: {
+    type: String,
+    required: [true, "email is required"],
+    minlength: 5,
+    unique: [true, "email address already exists"],
+    validate: [validator.isEmail, "invalid email address"],
+    lowercase: true,
+    trim: true
+  },
+  password: { type: String, required: true, minlength: 8, trim: true },
+  confirmPassword: {
+    type: String,
+    required: [true, "email is required"],
+    trim: true,
+    validate: {
+      validator(val) {
+        return val === this.password;
+      },
+      message: "passwords don't match"
+    }
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin", "employer"],
+    default: "user"
+  },
+  accountStatus: {
+    type: String,
+    enum: ["active", "inactive"],
+    default: "active"
+  }
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 8);
+  this.confirmPassword = undefined;
+  next();
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
